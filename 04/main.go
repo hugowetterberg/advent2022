20a5dd013ee08bb3b3da140d56e27b3d1b84725c
Hugo Wetterberg <hugo@wetterberg.nu>
2022-12-04 17:32:01 +0100

    day 4 part one

---
+++ 04/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("failed to run application: %v", err)
+		os.Exit(1)
+	}
+}
+
+type Range [2]int
+
+func (r Range) Contains(b Range) bool {
+	return r[0] <= b[0] && r[1] >= b[1]
+}
+
+func run() error {
+	var linum, containedPairs int
+
+	r := bufio.NewScanner(os.Stdin)
+
+	for r.Scan() {
+		line := bytes.NewReader(r.Bytes())
+
+		linum++
+
+		var a, b Range
+
+		_, err := fmt.Fscanf(line, "%d-%d,%d-%d",
+			&a[0], &a[1], &b[0], &b[1])
+		if err != nil {
+			return fmt.Errorf("failed to parse line %d: %w",
+				linum, err)
+		}
+
+		if a.Contains(b) || b.Contains(a) {
+			containedPairs++
+		}
+	}
+
+	err := r.Err()
+	if err != nil {
+		return fmt.Errorf("failed to read from stdin: %w", err)
+	}
+
+	fmt.Printf("assignement pairs where one fully contains the other: %d\n",
+		containedPairs)
+
+	return nil
+}
