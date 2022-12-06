666a493f1184d24e90f422f43b7aaeef71b76410
Hugo Wetterberg <hugo@wetterberg.nu>
2022-12-06 19:55:50 +0100

    day 6 solution one

---
+++ 06/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("failed to run application: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	seq := make([]byte, 4)
+
+	r := bufio.NewReader(os.Stdin)
+
+	n, err := r.Read(seq)
+	if err != nil {
+		return fmt.Errorf("failed to read first four bytes: %w", err)
+	}
+
+	for {
+		if uniq(seq) {
+			break
+		}
+
+		copy(seq, seq[1:])
+
+		b, err := r.ReadByte()
+		if err != nil {
+			return fmt.Errorf("failed to read next byte: %w", err)
+		}
+
+		n++
+		seq[3] = b
+	}
+
+	println("sequence is:", string(seq),
+		"found after reading", n, "characters")
+
+	return nil
+}
+
+func uniq(b []byte) bool {
+	for i := 0; i < len(b); i++ {
+		for j := i + 1; j < len(b); j++ {
+			if b[i] == b[j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
