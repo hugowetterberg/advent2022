16e817f1f8c3e2fb6e20138f95a231f10806bdc5
Hugo Wetterberg <hugo@wetterberg.nu>
2022-12-10 21:39:37 +0100

    day 10 solution one

---
+++ 10/main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("failed to run application: %v", err)
+		os.Exit(1)
+	}
+}
+
+type Machine struct {
+	X         int
+	Operation Operation
+	Failure   error
+}
+
+type Operation interface {
+	Tick(m *Machine) bool
+}
+
+type NoOp struct{}
+
+func (n NoOp) Tick(_ *Machine) bool {
+	return true
+}
+
+type AddX struct {
+	ticks int
+
+	Number int
+}
+
+func (op *AddX) Tick(m *Machine) bool {
+	op.ticks++
+
+	if op.ticks < 2 {
+		return false
+	}
+
+	m.X += op.Number
+
+	return true
+}
+
+type Exception struct {
+	Err error
+}
+
+func (op Exception) Tick(m *Machine) bool {
+	m.Failure = op.Err
+
+	return true
+}
+
+func run() error {
+	r := bufio.NewScanner(os.Stdin)
+
+	cancel := make(chan struct{})
+
+	defer close(cancel)
+
+	ops := make(chan Operation)
+
+	go readInput(r, ops, cancel)
+
+	m := Machine{
+		X: 1,
+	}
+
+	var sum int
+
+	for cycle := 1; true; cycle++ {
+		if m.Operation == nil {
+			in, ok := <-ops
+			if ok {
+				m.Operation = in
+			}
+		}
+
+		if cycle == 20 || (cycle > 20 && (cycle-20)%40 == 0) {
+			sum += cycle * m.X
+		}
+
+		if m.Operation == nil {
+			break
+		}
+
+		if m.Operation.Tick(&m) {
+			m.Operation = nil
+		}
+
+		if m.Failure != nil {
+			return m.Failure
+		}
+	}
+
+	println("sum", sum)
+
+	return nil
+}
+
+func readInput(
+	r *bufio.Scanner,
+	instructions chan Operation,
+	cancel chan struct{},
+) {
+	defer close(instructions)
+
+	var linum int
+
+	for r.Scan() {
+		line := r.Text()
+		linum++
+
+		op, err := parseOperation(line)
+		if err != nil {
+			instructions <- Exception{
+				Err: fmt.Errorf("parse operation %q on line %d: %w",
+					line, linum, err),
+			}
+
+			return
+		}
+
+		select {
+		case instructions <- op:
+		case <-cancel:
+			return
+		}
+	}
+
+	if err := r.Err(); err != nil {
+		instructions <- Exception{
+			Err: fmt.Errorf("failed to read input: %w", err),
+		}
+	}
+}
+
+func parseOperation(line string) (Operation, error) {
+	if line == "noop" {
+		return NoOp{}, nil
+	}
+
+	op, arg, _ := strings.Cut(line, " ")
+
+	switch op {
+	case "addx":
+		n, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, fmt.Errorf("invalid argument: %w", err)
+		}
+
+		return &AddX{
+			Number: n,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("unknown operation %q", op)
+}
